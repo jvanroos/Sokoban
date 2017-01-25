51e52a2c5b467bce1239d063237e1d3ead03fa74
jvanroos <jvanroos@gmail.com>
2017-01-25 21:07:13 +0100

    Delete TestKlasse.java

--- Sokuban/src/TestKlasse.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Fran on 24/01/2017.
- */
-public class TestKlasse {
-
-    //Fran heeft deze klasse aangemaakt in intelliJ om te testen of jullie veranderingen kunnen waarnemen.
-    // TEST geslaagd?
-}
