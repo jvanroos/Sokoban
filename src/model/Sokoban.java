cddb46282ab07d5b6ec7cae763ed1ca5da7cbe3b
jvanroos <jvanroos@gmail.com>
2017-01-25 22:37:14 +0100

    main class

---
+++ src/model/Sokoban.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by jvanroos on 25/01/2017.
+ */
+public class Sokoban {
+
+}
---
+++ src/model/TestSokoban.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Created by jvanroos on 25/01/2017.
+ */
+public class TestSokoban {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
