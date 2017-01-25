1290a9d5a3c9831a53aaa1001cf05d335f466385
FranValcke <fran.valcke@student.kdg.be>
2017-01-25 08:59:33 +0100

    mijn veranderingen
    testen

---
+++ Sokuban/src/TestKlasse.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Fran on 24/01/2017.
+ */
+public class TestKlasse {
+
+    //Fran heeft deze klasse aangemaakt in intelliJ om te testen of jullie veranderingen kunnen waarnemen.
+    // TEST geslaagd?
+}
---
+++ Sokuban/src/testInterface.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Fran on 25/01/2017.
+ */
+public interface testInterface {
+}
