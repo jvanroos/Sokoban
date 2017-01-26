75f82feb9fc30591badd2dd2b7a9a8387ffd28ff
FranValcke <fran.valcke@student.kdg.be>
2017-01-26 10:14:40 +0100

    Zelf een testklasse toegevooegd
    Is deze bij jullie er ook bijgekomen?

--- src/model/Sokoban.java
+++ src/model/Sokoban.java
@@ -4,8 +4,11 @@ package model;
  * Created by jvanroos on 25/01/2017.
  */
 public class Sokoban {
-    public Sokoban(){
 
-        System.out.println("Sokoban Constructor");
-    }
+      public Sokoban(){
+
+            System.out.println("Sokoban Constructor");
+        }
+
+
 }
---
+++ src/model/TestKlasseFran.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by Fran on 26/01/2017.
+ */
+public class TestKlasseFran {
+
+    //eens een klasse toevoegen om te testen of jullie deze kunnen zien!
+}
