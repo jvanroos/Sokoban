82bc9f5584d0b6623dad259e16fc78e17a09729b
jvanroos <jvanroos@gmail.com>
2017-01-29 11:03:09 +0100

    Enum toegevoegd

--- src/model/Sokoban.java
+++ src/model/Sokoban.java
@@ -1,24 +1,80 @@
 package model;
 
-/**
- * Created by jvanroos on 25/01/2017.
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import static model.Tiles.*;
+
 public class Sokoban {
     private Levels levels;
+    private int[][] cellStatus;	// array to copy the current level to
+    private int w;
+    private int h;
+    private Tiles[][] activeLevel;
     private int numLevels;
-    private int level;
+
 
     public Sokoban(String fileName){
-
-        level = 0;
+        // Create new Levels to access inner class methods
         levels = new Levels();
         numLevels = levels.setLevels(fileName);
 
-        int w;
-        int h;
+        createLevel(0);
 
+    }
 
+    public void createLevel(int level) {
+        h = levels.getHeight(level);
+        w = levels.getWidth(level);
+        activeLevel = new Tiles[w][h];
+        cellStatus = new int[w][h];
 
+        for(int i=0; i <h; i++){
+            for(int j = 0; j < w; j++){
+                activeLevel[j][i] = levels.getTile(level, j, i);
+                System.out.print(activeLevel[j][i] + ";");
+                switch(activeLevel[j][i]){
+                    case WALL:
+                        cellStatus[j][i] = WALL.ordinal();
+                        System.out.print("(" + cellStatus[j][i]+ ")");
+                        break;
+
+                    case BOX:
+                        cellStatus[j][i] = BOX.ordinal();
+                        System.out.print("(" + cellStatus[j][i]+ ")");
+                        break;
+
+                    case BOX_ON_GOAL:
+                        cellStatus[j][i] = BOX_ON_GOAL.ordinal();
+                        System.out.print("(" + cellStatus[j][i]+ ")");
+                        break;
+
+                    case GOAL:
+                        cellStatus[j][i] = GOAL.ordinal();
+                        System.out.print("(" + cellStatus[j][i]+ ")");
+                        break;
+
+                    case EMPTY:
+                        cellStatus[j][i] = EMPTY.ordinal();
+                        System.out.print("(" + cellStatus[j][i]+ ")");
+                        break;
+
+                    case PLAYER:
+                        cellStatus[j][i] = PLAYER.ordinal();
+                        System.out.print("(" + cellStatus[j][i]+ ")");
+                        break;
+
+                    case PLAYER_ON_GOAL:
+                        cellStatus[j][i] = PLAYER_ON_GOAL.ordinal();
+                        System.out.print("(" + cellStatus[j][i]+ ")");
+                        break;
+
+                }
+            }
+            System.out.println();
+        }
     }
 
 }
